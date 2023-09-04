c9ad05c2520613b975102906138ef9ee00e2126c
kyunghwa <snoopn303@gmail.com>
2023-09-04 15:49:02 +0900

    [DESIGN]tabLayout 기본 틀 작업

---
+++ app/src/main/java/com/example/contacts/MyPageFragment.kt
@@ -0,0 +1,11 @@
+package com.example.contacts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyPageFragment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mypage_fragment)
+    }
+}
