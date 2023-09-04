f99a720eb43c1ae9cdf7fd7713023081f2337bf8
shyr0809 <139097952+shyr0809@users.noreply.github.com>
2023-09-04 20:02:18 +0900

    commit

--- app/src/main/java/com/example/contacts/MainActivity.kt
+++ app/src/main/java/com/example/contacts/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.contacts
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.bumptech.glide.Glide
+import com.example.contacts.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
+
+
