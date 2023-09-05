12425567a7a85e27c4ee2174453ad4600f331a4e
shyr0809 <139097952+shyr0809@users.noreply.github.com>
2023-09-05 09:18:17 +0900

    feat

--- app/src/main/java/com/example/contacts/ContactFragment.kt
+++ app/src/main/java/com/example/contacts/ContactFragment.kt
@@ -1,29 +1,31 @@
+// ContactFragment.kt
 package com.example.contacts
 
+import android.app.AlertDialog
 import android.os.Bundle
-
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.LinearInterpolator
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.contacts.databinding.FragmentContactBinding
 
 class ContactFragment : Fragment() {
     private lateinit var binding: FragmentContactBinding
-    private val contactItems: MutableList<Contact> = mutableListOf()
     private lateinit var contactAdapter: ContactAdapter
     private var isGridMode = false
-    //text
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentContactBinding.inflate(inflater, container, false)
 
-        contactAdapter = ContactAdapter(contactItems, isGridMode)
+        contactAdapter = ContactAdapter(ContactsManager.contactsList, isGridMode)
         setLayoutManager()
 
         binding.gridBtn.setOnClickListener {
@@ -40,10 +42,54 @@ class ContactFragment : Fragment() {
 
         binding.RVArea.adapter = contactAdapter
 
-        contactItems.clear()
-        contactItems.addAll(ContactsManager.contactsList)
+        // [3]에서 floatingBtn을 클릭할 때 다이얼로그를 표시
+        binding.floatingBtn.setOnClickListener {
+            showAddContactDialog()
+        }
 
         return binding.root
+    }
+
+    // 다이얼로그를 표시하는 함수
+    private fun showAddContactDialog() {
+        val dialogView = LayoutInflater.from(requireContext()).inflate(R.layout.add_contact_dialog, null)
+        val dialog = AlertDialog.Builder(requireContext())
+            .setView(dialogView)
+            .create()
+
+        val nameEdit = dialogView.findViewById<EditText>(R.id.nameEdit)
+        val numberEdit = dialogView.findViewById<EditText>(R.id.numberEdit)
+        val emailEdit = dialogView.findViewById<EditText>(R.id.eMailEdit)
+        val eventEdit = dialogView.findViewById<EditText>(R.id.eventEdit)
+
+        dialogView.findViewById<Button>(R.id.saveBt)?.setOnClickListener {
+            val name = nameEdit.text.toString()
+            val phoneNumber = numberEdit.text.toString()
+            val email = emailEdit.text.toString()
+            val event = eventEdit.text.toString()
+
+            // 필수 정보가 입력되었는지 확인
+            if (name.isNotEmpty() && phoneNumber.isNotEmpty() && email.isNotEmpty()&& event.isNotEmpty()) {
+                // Contact로 사용자 입력 정보 전달
+                ContactsManager.addContact(name, phoneNumber, email, event)
+
+                // 다이얼로그 닫기
+                dialog.dismiss()
+
+                // RecyclerView 업데이트
+                contactAdapter.notifyDataSetChanged()
+            } else {
+                // 필수 정보가 입력되지 않은 경우 토스트 메시지 표시
+                Toast.makeText(requireContext(), "입력되지 않은 정보가 있습니다", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        dialogView.findViewById<Button>(R.id.cancelBt)?.setOnClickListener {
+            // 다이얼로그 닫기
+            dialog.dismiss()
+        }
+
+        dialog.show()
     }
 
     private fun setLayoutManager() {
@@ -54,4 +100,3 @@ class ContactFragment : Fragment() {
         }
     }
 }
-
--- app/src/main/java/com/example/contacts/ContactManager.kt
+++ app/src/main/java/com/example/contacts/ContactManager.kt
@@ -20,6 +20,9 @@ init {
     contactsList.addAll(listOf( contact1,contact2,contact3,contact4,contact5,contact6,contact7,contact8,contact9,contact10,contact11,contact12))
 }
 
-
+    fun addContact(name: String, phoneNumber: String, email: String, event: String) {
+        val newContact = Contact(name, phoneNumber, email, R.drawable.me, false)
+        contactsList.add(newContact)
+    }
 
 }
