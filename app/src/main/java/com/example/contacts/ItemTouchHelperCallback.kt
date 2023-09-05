98647ef66d7acdeb102460efdd3e81f36affbd9d
kyunghwa <snoopn303@gmail.com>
2023-09-05 16:45:17 +0900

    [DESIGN]:기타 디자인 수정작업

--- app/src/main/java/com/example/contacts/ItemTouchHelperCallback.kt
+++ app/src/main/java/com/example/contacts/ItemTouchHelperCallback.kt
@@ -1,8 +1,12 @@
 package com.example.contacts
 
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
 import android.graphics.drawable.ColorDrawable
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.ColorUtils
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 
@@ -23,4 +27,34 @@ class ItemTouchHelperCallback(
     }
 
     // 스와이프 디자인
+    override fun onChildDraw(
+        c: Canvas,
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        dX: Float, dY: Float,
+        actionState: Int,
+        isCurrentlyActive: Boolean
+    ) {
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+
+        val itemView = viewHolder.itemView
+
+        // 스와이프 색상 지정
+        val color = ContextCompat.getColor(itemView.context, R.color.light_main)
+
+        // 스와이프 중에만 아이템의 일부에 배경색을 그려주는 코드
+        if (dX > 0) { // 오른쪽으로 스와이프하는 경우
+            val paint = Paint()
+            paint.color = color
+
+            val background = RectF(
+                itemView.left.toFloat(),
+                itemView.top.toFloat(),
+                itemView.left + dX,
+                itemView.bottom.toFloat()
+            )
+
+            c.drawRect(background, paint)
+        }
+    }
 }
