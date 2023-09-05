5b21fbd41b690951903f5e55292f7a06eaaf9907
bora park <jemini9595@gmail.com>
2023-09-06 02:04:36 +0900

    [Feat] : Detail에서 북마크할 시 Contact프래그에도 전달해서 UI적용

--- app/src/main/java/com/example/contacts/DetailActivity.kt
+++ app/src/main/java/com/example/contacts/DetailActivity.kt
@@ -4,18 +4,22 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Messenger
 import com.example.contacts.Util.callPhoneNumber
 
 import com.example.contacts.databinding.ActivityDetailBinding
+import com.google.android.material.snackbar.Snackbar
 
 class DetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailBinding
 
     companion object {
         private lateinit var detailContact: Contact
+
         fun newIntentForDetail(context: Context?, contact: Contact) =
             Intent(context, DetailActivity::class.java).apply {
                 detailContact = contact
+
             }
     }
 
@@ -30,6 +34,31 @@ class DetailActivity : AppCompatActivity() {
             tvName.text = detailContact.name
             ivUser.setImageResource(detailContact.photo)
 
+            if (detailContact.bookmark){
+                binding.bookmark.setBackgroundResource(R.drawable.clicked_bookmark)
+            }else{
+                binding.bookmark.setBackgroundResource(R.drawable.unclicked_bookmark)
+            }
+
+            binding.bookmark.setOnClickListener {
+
+                detailContact.bookmark = !detailContact.bookmark
+
+
+                if (detailContact.bookmark){
+                    binding.bookmark.setBackgroundResource(R.drawable.clicked_bookmark)
+                    showSnackBarMessage("                                      ⭐즐찾⭐")
+                }else{
+                    binding.bookmark.setBackgroundResource(R.drawable.unclicked_bookmark)
+                    showSnackBarMessage("                                   ⭐즐찾해제⭐")
+                }
+                val resultIntent = Intent()
+                resultIntent.putExtra("BOOKMARK", detailContact.bookmark)
+
+                setResult(RESULT_OK,resultIntent)
+
+            }
+
             btnCancel.setOnClickListener {
                 finish()
             }
@@ -39,4 +68,9 @@ class DetailActivity : AppCompatActivity() {
             }
         }
     }
+    fun showSnackBarMessage(message: String){
+        val snackbar = Snackbar.make(binding.root,message,Snackbar.LENGTH_SHORT)
+        snackbar.show()
+    }
+
 }
